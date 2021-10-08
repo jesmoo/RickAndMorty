0c3ed0ab33fa5d81aec3b18fd2b04f90dd6994a3
jesmoo <habidmoo@gmail.com>
2021-10-08 17:22:38 -0500

    :children_crossing: 404
    Added a 404 page, in case the user enters where something does not exist.

--- src/components/Notfound.jsx
+++ src/components/Notfound.jsx
@@ -1,10 +1,21 @@
 import React from 'react';
-
+import { Link } from 'react-router-dom';
+import icon404 from '../assets/icons/fourohfour.svg';
+import '../styles/components/NotFound.css';
 const Notfound = () => {
   return (
-    <div>
-      <h1>Not foun</h1>
-    </div>
+    <main className="svgcontainer">
+      <div className="subcont">
+        <div className="topconvo">
+          <Link to="/" className="Character__comeBack" />
+          <span>!@#$</span>
+          <br />
+          your portal gun must be busted...
+        </div>
+        <img className="icon404" src={icon404} alt="404" />
+        <div className="bottomconvo">Page not found. Wubba Lubba Dub Dub.</div>
+      </div>
+    </main>
   );
 };
 
